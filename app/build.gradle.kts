7eab6e252fb40df194aa9868f659ab9d6de50482
Fujiaix001 <307279363+Fujiaix001@users.noreply.github.com>
2026-08-02 00:40:16 +0800

    chore: identify selectable size modes release as 4.0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val versionMajor = 4
 val versionMinor = 0
-val basePatch = 16
+val basePatch = 17
 val appVersionCode = (versionMajor * 10_000) + (versionMinor * 100) + basePatch
 val releasePropertiesFile = rootProject.file("keystore.properties")
 val releaseProperties = Properties().apply {
