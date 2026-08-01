42a65789750a5e7cac03e6d62cd9e7fc38d57357
Fujiaix001 <307279363+Fujiaix001@users.noreply.github.com>
2026-08-02 00:40:03 +0800

    chore: identify draggable clock release as 4.0.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val versionMajor = 4
 val versionMinor = 0
-val basePatch = 15
+val basePatch = 16
 val appVersionCode = (versionMajor * 10_000) + (versionMinor * 100) + basePatch
 val releasePropertiesFile = rootProject.file("keystore.properties")
 val releaseProperties = Properties().apply {
